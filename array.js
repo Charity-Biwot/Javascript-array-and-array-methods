70eb25896fc5d07fea986cee4ba6f822466e4ba9
Charity-Biwot <biwotcharityjerop@gmail.com>
2022-06-30 19:29:26 +0300

    commit

---
+++ array.js
@@ -0,0 +1,5 @@
+//  Create a new array whose elements is in uppercase of words present in the original array.
+
+
+let words = ["eating","coding","jumping"]
+console.log(words.map(word => word.toUpperCase()))
---
+++ index.js
@@ -0,0 +1,48 @@
+var x = [1,2,3,"joy","Happiness","love"]
+var len = x.length;
+x[len - 1];
+console.log(len)
+
+//create a function called function 1 and create another function that takes 
+// function one(){
+//     x = 500
+//     function two(){
+//         console.log(x)
+//     }
+// }
+// one
+// two
+// main();
+// function main(){
+//     x(12)
+// }
+
+// function x(number){
+//     var num = 1+number;
+//     console.log(num)
+// }
+//write a javascript program to check the grades of a student in examinations
+//student has marks btn 9o to 100.
+//you will print out the students grade in A
+
+function totalgrade(marks)
+  {
+  if (MARKS>=90 && marks<=100) {
+    console.log("A");
+  }
+ elseif (marks>=70 && marks<=89) 
+  console.log("B")
+
+  }
+  elseif (marks>=60 && marks<=69)
+
+ }
+ elseif (marks>=60 && marks<=69)
+
+ }
+  
+ 
+ 
+
+;
+
